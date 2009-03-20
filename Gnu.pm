bd53f18f6bc0095e184b992476e79e5320c7b1d8
Hiroo HAYASHI <hiroo.hayashi@computer.org>
2009-03-20 17:00:37 +0000

    Import of HAYASHI/Term-ReadLine-Gnu-1.19 from CPAN.
    gitpan-cpan-distribution: Term-ReadLine-Gnu
    gitpan-cpan-version:      1.19
    gitpan-cpan-path:         HAYASHI/Term-ReadLine-Gnu-1.19.tar.gz
    gitpan-cpan-author:       HAYASHI
    gitpan-cpan-maturity:     released

--- Gnu.pm
+++ Gnu.pm
@@ -1,7 +1,7 @@
 #
 #	Gnu.pm --- The GNU Readline/History Library wrapper module
 #
-#	$Id: Gnu.pm,v 1.98 2009/02/27 12:46:45 hiroo Exp $
+#	$Id: Gnu.pm,v 1.100 2009/03/20 16:33:25 hiroo Exp $
 #
 #	Copyright (c) 2009 Hiroo Hayashi.  All rights reserved.
 #
@@ -73,7 +73,7 @@ END
     use DynaLoader;
     use vars qw($VERSION @ISA @EXPORT_OK);
 
-    $VERSION = '1.18';
+    $VERSION = '1.19';
 
     # Term::ReadLine::Gnu::AU makes a function in
     # `Term::ReadLine::Gnu::XS' as a method.
@@ -291,6 +291,11 @@ $Term::ReadLine::registered = $Term::ReadLine::registered;
 sub readline {			# should be ReadLine
     my $self = shift;
     my ($prompt, $preput) = @_;
+
+    # contributed fix for Perl debugger
+    # make sure the outstream fd inside the readline library is
+    # in sync (see http://bugs.debian.org/236018)
+    $Attribs{outstream} = $Attribs{outstream};
 
     # ornament support (now prompt only)
     $prompt = ${$Attribs{term_set}}[0] . $prompt . ${$Attribs{term_set}}[1];
--- t/00checkver.t
+++ t/00checkver.t
@@ -1,9 +1,9 @@
 # -*- perl -*-
 #	00check.t - check versions
 #
-#	$Id: 00checkver.t,v 1.1 2008-03-20 22:50:26+09 hiroo Exp hiroo $
+#	$Id: 00checkver.t,v 1.2 2009/02/27 14:16:14 hiroo Exp $
 #
-#	Copyright (c) 2008 Hiroo Hayashi.  All rights reserved.
+#	Copyright (c) 2009 Hiroo Hayashi.  All rights reserved.
 #
 #	This program is free software; you can redistribute it and/or
 #	modify it under the same terms as Perl itself.
